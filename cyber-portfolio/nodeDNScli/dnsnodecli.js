ccf405000d732d364040197737d22cbffa376c8d
El Gabro <gabriele.bellavia.m@gmail.com>
2022-05-23 14:22:24 +0200

    Added utility functions to dns script

--- cyber-portfolio/nodeDNScli/dnsnodecli.js
+++ cyber-portfolio/nodeDNScli/dnsnodecli.js
@@ -3,21 +3,53 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const dig = require('node-dig-dns');
-const netstat = require('node-netstat');
 
-fs.readFile('rootservers.json', 'utf8', (err, data) => {
-    if(err) {
-        console.error(err);
+
+const rawdata = fs.readFileSync('rootservers.json');
+const rootservers = JSON.parse(rawdata);
+const domainToSolve = process.argv[2];
+
+
+function makeChoiceList(jsonResponse){
+    let choices = [];
+    for (var entry of jsonResponse){
+	if('value' in entry){ // using this function also for DIG
+            choices.push(`${entry['domain']}  ${entry['value']}`);
+        }
+	else{ // this one for rootservers.json file
+            choices.push(`${entry['hostname']}  ${entry['ipv4']}`);
+	}
     }
-    console.log(data);
-});
+    return choices;
+}
 
 
-inquirer
-.prompt([
-	{
-	  type : "list",
-          name : "serverdns",
-          message: "Select root dns server",
-          choices : ["nessuno", "a", "b", "c", "d"]
+function makePrompt(choiceList) {
+    inquirer.prompt([{
+	type : "list",
+        name : "receiver",
+        message: "To whom do you want to talk?",
+        choices : choiceList,
         }])
+}
+
+
+// const res = await makePrompt();
+// res.receiver
+
+function digCall(host, domainName){
+    dig([`@${host}`, `${domainName}`]).then(res => {
+    if ('additional' in res) {
+        console.log(res.additional);
+      }
+    else if ('answer' in res){
+        console.log('FOUND: ', res.answer);
+      }
+    else {
+        throw new Error("Error. Something has happened. Try again.");
+      }
+    })
+}
+
+
+
